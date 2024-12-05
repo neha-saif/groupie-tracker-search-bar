954f9572a221050b53dc7d8a764f000c3aae7552
neha-saif <160529296+neha-saif@users.noreply.github.com>
2024-12-05 17:11:46 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"groupie/functions"
+	"html/template" //text
+	"log"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
-	"text/template"
 )
+
+// hfgwhkuef
 
 type Final struct {
 	ID        int
 	Image     string
 	Artist    string
-	Members   string
+	Members   functions.Members
 	AlbumYear int
 	Album1    string
 	Locations []string
@@ -22,20 +26,94 @@ type Final struct {
 func main() {
 	// handler functions
 	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
-	// http.HandleFunc("/homepagesearch", homepagesearch)
+	http.HandleFunc("/homepagesearch", homepagesearch)
 	http.HandleFunc("/result", result)
 	http.HandleFunc("/", homepage)
-
 	http.ListenAndServe(":8080", nil)
 }
-
-func homepage(w http.ResponseWriter, r *http.Request) {
+func homepagesearch(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+		return
+	}
+	// where form value is collected for artist name annd fed innto the relavent funnction
+	artIdsug := r.FormValue("suggestions[]")
+	fmt.Println("artIdsug:", artIdsug)
+	var ids []int
 
 	character, _ := functions.LoadData("https://groupietrackers.herokuapp.com/api/artists")
+
+for _, data := range character {
+		fmt.Print(string(data.Artist))
+		if strings.Contains(string(data.Artist), artIdsug) {
+			ids = append(ids, data.ID)
+
+	} 
+	for _, mem := range data.Members {
+		if strings.Contains(mem,artIdsug) {
+			ids = append(ids, data.ID)
+		}
+	// if strings.Contains(data.Album1,artIdsug) {
+	// 	ids = append(ids, data.ID)
+	// }
+	// if strings.Contains(strconv.Itoa(data.AlbumYear),artIdsug)  {
+	// 	ids = append(ids, data.ID)
+	// }
+	
+}
+}
+	// make thhis
+
+	// for _, sug := range artIdsug {
+	// 	parts := strings.Split(string(sug), ":")
+	// 	if len(parts) > 1 {
+	// 		id, err := strconv.Atoi(strings.TrimSpace(parts[len(parts)-1]))
+	// 		if err == nil {
+	// 			ids = append(ids, id)
+	// 		}
+	// 	}
+	// }
+
+	fmt.Println("ids:",ids)
+	
+
+
+// Filter char by IDs
+filterchar := []functions.Data{}
+	for _, id := range ids {
+		for _, data := range character {
+			if data.ID == id {
+				filterchar = append(filterchar, data)
+			}
+		}
+	}
+
 
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, "Error parsing html", http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, filterchar)
+	if err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		renderErrorPage(w, 404)
+		return
+	}
+	character, _ := functions.LoadData("https://groupietrackers.herokuapp.com/api/artists")
+
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		renderErrorPage(w, 500)
 		return
 	}
 
@@ -46,17 +124,33 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func result(wr http.ResponseWriter, r *http.Request) {
+	// For resultpage the request is always POST not GET
+	if r.Method != http.MethodPost {
+		renderErrorPage(wr, 405)
+		return
+	}
+
+	// if url is not for result page error handle
+	if r.URL.Path != "/result" {
+		renderErrorPage(wr, 404)
+		return
+	}
+
 	char, _ := functions.LoadData("https://groupietrackers.herokuapp.com/api/artists")
 	artId := r.FormValue("artist")
-	fmt.Println("artId:", artId)
+	for _, ch := range artId {
+		if ch != 10 && ch != 13 && (ch < 32 || ch > 126) {
+			renderErrorPage(wr, 400)
+			return
+		}
+	}
 	artId = strings.Title(artId)
 
 	if len(artId) > 2 {
-artId = string(artId[0])
+		pattern := regexp.MustCompile(`:\d+`)
+		artId = strings.TrimPrefix(pattern.FindString(artId), ":")
 	}
-	fmt.Println("artId:", artId)
 
 	for i := 0; i < 52; i++ {
 		if artId == char[i].Artist {
@@ -64,11 +158,9 @@ artId = string(artId[0])
 		}
 	}
 
-	fmt.Println("artId:", artId)
-
 	iint, err := strconv.Atoi(artId)
 	if err != nil || iint <= 0 {
-		http.Error(wr, "Invalid artist ID", http.StatusBadRequest)
+		renderErrorPage(wr, 500)
 		return
 	}
 	i := iint - 1
@@ -96,16 +188,16 @@ artId = string(artId[0])
 		return
 	}
 
-	members := "No members available"
-	if len(character[i].Members) > 0 {
-		members = strings.Join(character[i].Members, ", ")
-	}
+	// members := "No members available"
+	// if len(character[i].Members) > 0 {
+	// 	members = strings.Join(character[i].Members, ", ")
+	// }
 
 	var cdata []string
-	x := '1'
+	x := 1
 	d := ""
 	for location, date := range charData[i].DatesLocations {
-		d = string(x) + ") " + strings.ReplaceAll(string(location), "-", ", ") + ": " + strings.Join(date, ", ")
+		d = strconv.Itoa(x) + ") " + strings.ReplaceAll(string(location), "-", ", ") + ": " + strings.Join(date, ", ")
 		d = strings.ReplaceAll(d, "_", " ")
 		cdata = append(cdata, d)
 		x++
@@ -115,20 +207,53 @@ artId = string(artId[0])
 		ID:        character[i].ID,
 		Image:     character[i].Image,
 		Artist:    character[i].Artist,
-		Members:   members,
+		Members:   functions.Members{},
 		AlbumYear: character[i].AlbumYear,
 		Album1:    character[i].Album1,
 		Locations: cdata,
 	}
 
+	MMember := []string{}
+	MMember = append(MMember, character[i].Members...)
+
+	FFinal.Members = MMember
+
 	t, err := template.ParseFiles("result.html")
 	if err != nil {
-		http.Error(wr, "Error parsing result.html template", http.StatusInternalServerError)
+		renderErrorPage(wr, 500)
 		return
 	}
 
 	errr := t.Execute(wr, FFinal)
 	if errr != nil {
+		log.Println(errr)
 		http.Error(wr, "Error executing template", http.StatusInternalServerError)
+	}
+}
+
+func renderErrorPage(w http.ResponseWriter, code int) {
+
+	// Set the status coded
+	if code == 500 {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else if code == 404 {
+		w.WriteHeader(http.StatusNotFound)
+	} else if code == 400 {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	// Generate ASCII art for the error code with the "Standard" style
+
+	// Parse and render the custom 404 template
+	t, err := template.ParseFiles(fmt.Sprintf("style/%d.html", code))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing %d HTML", code), http.StatusInternalServerError)
+		return
+	}
+
+	// Render the template with the result
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error executing %d template", code), http.StatusInternalServerError)
 	}
 }
